a758ca32a3e1c86d8f215d4a9ea6e979e6a09506
jhessiny <jhessinymattos@gmail.com>
2021-03-06 12:31:57 -0300

    getting system msgs

--- src/components/Chatroom.jsx
+++ src/components/Chatroom.jsx
@@ -1,9 +1,62 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import UserMessageBallon from "./UserMessageBallon";
 import SystemMessageBallon from "./SystemMessageBallon";
 
 function App() {
+  const states = [
+    "AC",
+    "AL",
+    "AM",
+    "AP",
+    "BA",
+    "CE",
+    "DF",
+    "ES",
+    "GO",
+    "MA",
+    "MT",
+    "MS",
+    "MG",
+    "PA",
+    "PB",
+    "PR",
+    "PE",
+    "PI",
+    "RJ",
+    "RN",
+    "RO",
+    "RS",
+    "RR",
+    "SC",
+    "SE",
+    "SP",
+    "TO",
+  ];
   const [messages, setMessages] = useState(["oi", "oi"]);
+  const [systenMessages, setSystenMessages] = useState([]);
+  const [userMessages, setUserMessages] = useState([
+    { name: "name", type: "text", placeholder: "Nome e sobrenome" },
+    { name: "address", states: [...states], cities: [] },
+    { name: "birthday", type: "date", placeholder: "" },
+    { name: "email", type: "email", placeholder: "E-mail" },
+    { name: "rating", type: "email", placeholder: "rating" },
+  ]);
+
+  useEffect(() => {
+    let mymsgs = [];
+    axios
+      .get("https://6041700df34cf600173c9dba.mockapi.io/systemMsgs")
+      .then((data) => {
+        setSystenMessages(data.data);
+        mymsgs.push(data.data[0]);
+        mymsgs.push(userMessages[0]);
+      })
+      .then(() => {
+        setMessages(mymsgs);
+      });
+  }, [userMessages]);
+
   return (
     <div className="App">
       <div className="chat-room">
@@ -18,7 +71,7 @@ function App() {
               (index + 1) % 2 === 0 ? (
                 <UserMessageBallon />
               ) : (
-                <SystemMessageBallon />
+                <SystemMessageBallon msg={msg} />
               )
             )}
           {messages.length >= 10 && (
--- src/components/SystemMessageBallon.jsx
+++ src/components/SystemMessageBallon.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function SystemMessageBallon({ msg, user }) {
-  return <div className="box ballon1">System msg</div>;
+  return (
+    <div className="box ballon1">
+      {" "}
+      {msg.content.length > 1
+        ? msg.content[0] + user.name + msg.content[1]
+        : msg.content[0]}
+    </div>
+  );
 }
 
 export default SystemMessageBallon;
